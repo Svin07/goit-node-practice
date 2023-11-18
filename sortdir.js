1048df3e1cf907583dacb733f971c319b3343d39
Liudmila-Ridko <125599487+liussi@users.noreply.github.com>
2023-11-18 14:50:22 +0200

    hendlerFile

--- sortdir.js
+++ sortdir.js
@@ -1,8 +1,10 @@
 const path = require("path");
 const fs = require("fs/promises");
+let dirBasePath = null;
 
 function getBasePath(filePath) {
   const basePath = path.join(__dirname, filePath);
+  dirBasePath = basePath;
   return basePath;
 }
 
@@ -15,19 +17,45 @@ async function sortDir(filePath) {
       const itemPath = path.join(filePath, item);
       const stat = await fs.stat(itemPath);
       if (stat.isFile()) {
-        console.log("HandleFile>>>");
-        // handleFile();
+        handleFile(itemPath);
       } else {
-        console.log("Handle Dir>>>");
         sortDir(itemPath);
       }
     });
-    console.log(dirList);
   } catch (error) {
     console.log("sortdir error ->", error.message);
   }
 }
 
-function handleFile(params) {}
+const fileGroups = {
+  IMAGES: ["JPEG", "PNG", "JPG", "SVG"],
+  VIDEO: ["AVI", "MP4", "MOV", "MKV"],
+  DOCUMENTS: ["DOC", "DOCX", "TXT", "PDF", "XLSX", "PPTX"],
+  MUSIC: ["MP3", "OGG", "WAV", "AMR"],
+  ARCHIVES: ["ZIP", "GZ", "TAR"],
+  UNDEFINED: [],
+};
 
+async function handleFile(filePath) {
+  const fileExt = path.extname(filePath).slice(1).toUpperCase();
+  const dirBaseList = await fs.readdir(dirBasePath);
+
+  for (const group in fileGroups) {
+    if (fileGroups[group].includes(fileExt)) {
+      if (!dirBaseList.includes(group)) {
+        await fs.mkdir(path.join(dirBasePath, group));
+      }
+      await fs.rename(filePath,)
+    }
+
+    if (group === "UNDEFINED") {
+      if (!dirBaseList.includes(group)) {
+        await fs.mkdir(path.join(dirBasePath, group));
+      }
+      console.log("move to undefined");
+    }
+
+
+  }
+}
 module.exports = { sortDir, getBasePath };
